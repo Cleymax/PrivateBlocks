80c3b29a7dbfc291c931394dcc54ee99fcd689bd
Clément P. (Cleymax) <clemperrin1@gmail.com>
2020-01-03 17:42:17 +0100

    :tada: Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+A
+gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+i
+9src/main/java/fr/cleymax/privateblocks/PrivateBlocks.java,a\1\a13a84d5631148eb41d23c1b0d852b6a86c39114
---
+++ src/main/java/fr/cleymax/privateblocks/PrivateBlocks.java
@@ -0,0 +1,26 @@
+package fr.cleymax.privateblocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * File <b>PrivateBlocks</b> located on fr.cleymax.privateblocks is a part of PrivateBlocks.
+ * <p>
+ * Copyright (c) 2020 Cleymax.
+ * <p>
+ *
+ * @author Clément P. (Cleymax), {@literal <contact@cleymax.fr>} Created the 03/01/2020
+ */
+public final class PrivateBlocks extends JavaPlugin {
+
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+}
